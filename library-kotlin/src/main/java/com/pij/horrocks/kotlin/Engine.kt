4e6c618abd96d03aa51eca49a3c6db2c3cd11417
pijpijpij <atlassian@champault.co.uk>
2020-10-04 18:04:46 +0100

    Made logger and foreground scheduler optional.

--- library-kotlin/src/main/java/com/pij/horrocks/kotlin/Engine.kt
+++ library-kotlin/src/main/java/com/pij/horrocks/kotlin/Engine.kt
@@ -8,40 +8,37 @@ import org.slf4j.LoggerFactory
 
 class Engine<S : Any>(
         initialState: S,
-        private val clearEventProperties: S.() -> S,
-        foreground: Scheduler,
-        private val logger: Logger,
+        private val clearEventProperties: S.() -> S = { this },
+        foreground: Scheduler? = null,
+        private val logger: Logger? = Companion.logger,
         private vararg val features: Feature<*, S>
 ) {
-    constructor(
-            initialState: S,
-            clearEventProperties: S.() -> S,
-            foreground: Scheduler,
-            vararg features: Feature<*, S>
-    ) : this(initialState, clearEventProperties, foreground, Companion.logger, *features)
 
     companion object {
-        private val logger = LoggerFactory.getLogger(Engine::class.java)
+        private val logger: Logger by lazy { LoggerFactory.getLogger(Engine::class.java) }
 
-        fun <T> Observable<T>.subscribeSafely(display: (T) -> Unit, stallingAction: (Throwable) -> Unit = {}): Disposable = this
+        fun <S : Any> createWithLogger(
+                initialState: S,
+                clearEventProperties: S.() -> S,
+                foreground: Scheduler,
+                vararg features: Feature<*, S>
+        ) = Engine(initialState, clearEventProperties, foreground, features = features)
+
+        fun <T> Observable<T>.subscribeSafely(display: (T) -> Unit, logger: Logger? = Companion.logger, stallingAction: (Throwable) -> Unit = {}): Disposable = this
                 .subscribe({
                     try {
                         display(it)
                     } catch (e: Throwable) {
-                        logger.warn("Failure while displaying '$it'", e)
+                        logger?.warn("Failure while displaying '$it'", e)
                     }
                 }, {
-                    logger.error("Engine failure", it)
+                    logger?.error("Engine failure", it)
                     try {
                         stallingAction(it)
                     } catch (ignored: Throwable) {
-                        logger.error("Could not perform a last ditch action before stalling.", ignored)
+                        logger?.error("Could not perform a last ditch action before stalling.", ignored)
                     }
                 })
-    }
-
-    init {
-        require(features.isNotEmpty()) { "Must provide a least one feature to spin" }
     }
 
     /**Only visible to offer some form of backward compatibility. */
@@ -49,21 +46,26 @@ class Engine<S : Any>(
     @Suppress("unused")
     val reducers = reducers()
     private fun reducers() = features.map {
-        it.reducers.doOnError { error -> logger.warn("failed to emit a reducer", error) }
+        it.reducers.doOnError { error -> logger?.warn("failed to emit a reducer", error) }
     }
 
-    val states: Observable<S> = Observable.merge(reducers())
-            .scan(initialState) { state, reducer: Reducer<S> -> reducer.reduceSafely(state) }
-            .doOnNext { logger.debug("${this.javaClass} $it") }
-            .doAfterNext { state -> features.forEach { it.context.onNext(state) } }
-            .observeOn(foreground)
+    val states: Observable<S>
+
+    init {
+        require(features.isNotEmpty()) { "Must provide a least one feature to spin" }
+
+        states = Observable.merge(reducers())
+                .scan(initialState) { state, reducer: Reducer<S> -> reducer.reduceSafely(state) }
+                .run { if (logger == null) this else doOnNext { logger.debug("$javaClass $it") } }
+                .doAfterNext { state -> features.forEach { it.context.onNext(state) } }
+                .run { if (foreground == null) this else observeOn(foreground) }
+    }
 
     private fun Reducer<S>.reduceSafely(state: S) = try {
         invoke(state.clearEventProperties())
     } catch (e: Throwable) {
-        logger.warn("Failed reducer", e)
+        logger?.warn("$javaClass Failed reducer", e)
         throw e
     }
-
 
 }
